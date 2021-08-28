39f2506237eb0a1884335ee7ee536188e03446b3
Helia <helia@m.lo.agency>
2021-08-28 21:15:31 +0430

    fix

--- server.js
+++ server.js
@@ -11,23 +11,28 @@ const server = http.createServer((req, res) => {
     path = "index.html";
   }
   console.log(`Requested path ${path} `);
-
   let file = __dirname + "/public/" + path;
-  fs.readFile(file, function(err, content) {
+
+  if (!fs.existsSync(file)){
+    file = __dirname +"/public/error.html";
+  }
+  
+  fs.readFile(file, function(err, data) {
     if (err) {
       console.log(`File Not Found ${file}`);
       res.writeHead(404);
       res.end();
     } else {
+      
       console.log(`Returning ${path}`);
-      res.setHeader("X-Content-Type-Options", "nosniff");
+      res.setHeader("Content-Type", "nosniff");
       let mime = lookup(path);
       res.writeHead(200, {"content-type":mime});
-      res.end(content);
+      res.end(data);
     }
   });
 });
 
 server.listen(3000, "localhost", () => {
-  console.log("Listening on port 1234");
+  console.log("Listening on port 3000");
 });
